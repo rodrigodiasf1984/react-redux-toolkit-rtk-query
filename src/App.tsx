92ed69329fcffde1152abc252133a9c8bec09eb6
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-10-01 17:55:38 +0200

    add store to use with rtk

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import "./App.css";
+import { useGetUsersQuery } from "./services/users";
 
 function App() {
+  const { data, error, isLoading, isSuccess, isError } =
+    useGetUsersQuery("teste");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <h1>Welcome to React Redux Toolkit RTK Query</h1>
+      {isLoading && "Loading..."}
+      {isError && error}
+      {isSuccess &&
+        data &&
+        data.map((user: { id: number; name: string }) => (
+          <h1 key={user.id}>{user.name}</h1>
+        ))}
     </div>
   );
 }
